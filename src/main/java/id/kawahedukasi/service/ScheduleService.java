8b702c73f39e414a706571def4f9e1ee7640068a
AnnisaDwiF <annisadwif92@gmail.com>
2023-03-12 21:23:42 +0700

    add scheduler

---
+++ src/main/java/id/kawahedukasi/service/ScheduleService.java
@@ -0,0 +1,33 @@
+package id.kawahedukasi.service;
+
+import io.quarkus.arc.properties.IfBuildProperty;
+import io.quarkus.scheduler.Scheduled;
+import io.vertx.core.impl.logging.Logger;
+import io.vertx.core.impl.logging.LoggerFactory;
+import net.sf.jasperreports.engine.JRException;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.Date;
+
+@ApplicationScoped
+public class ScheduleService {
+
+    @Inject
+    MailService mailService;
+
+    Logger logger = LoggerFactory.getLogger(ScheduleService.class);
+    @Scheduled(every = "10s")
+    public void generateKawahEdukasi(){
+        logger.info("KawahEdukasi");
+    }
+
+    @Scheduled(cron = "* * 21 * * ?")
+    public void generateSendEmailListPeserta() throws JRException, IOException {
+        mailService.sendExcelToEmail("annisadwifebryantipnp@gmail.com");
+        logger.info("send email success!");
+
+    }
+}
